15f4e6d76e6845e8a3ffc29bb40263346a3838a0
eastar <eastar.j@navercorp.com>
2021-11-19 17:07:16 +0900

    1 week step 6-3

--- week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
@@ -1,19 +1,22 @@
 package dev.eastar.codelab.basicscodelab
 
+import android.annotation.SuppressLint
+import android.log.Log
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
 import dev.eastar.codelab.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -36,21 +39,41 @@ fun MyApp(names: List<String> = listOf("World", "Compose")) {
     }
 }
 
+@SuppressLint("UnrememberedMutableState")
 @Composable
 fun Greeting(name: String) {
+    Log.e(name)
+    val expanded = remember { mutableStateOf(false) }
+    Log.e()
+
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Column(
-            Modifier
-                .fillMaxWidth()
-                .padding(24.dp)
-        ) {
-            Text("Hello")
-            Text(name)
+        Log.e()
+        Row(Modifier.padding(24.dp)) {
+            Log.e()
+            Column(
+                Modifier.weight(1F)
+            ) {
+                Log.e()
+                Text("Hello")
+                Text(name)
+            }
+            Log.e()
+
+            OutlinedButton(onClick = {
+                Log.e()
+                expanded.value = !expanded.value
+            }) {
+                Log.e()
+                Text(text = if (expanded.value) "Show less" else "Show more")
+            }
+            Log.e()
         }
+        Log.e()
     }
+    Log.e()
 }
 
 
