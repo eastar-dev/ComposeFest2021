40be9ad358a46fb5e739b4cc2d99d568ed83786a
eastar <eastar.j@navercorp.com>
2021-11-19 19:00:02 +0900

    1 week step 9

--- week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -65,9 +67,9 @@ fun OnboardingPreview() {
 }
 
 @Composable
-fun Greetings(names: List<String> = listOf("World", "Compose")) {
-    Column {
-        for (name in names) {
+private fun Greetings(names: List<String> = List(1000) { "$it" }) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
+        items(items = names) { name ->
             Greeting(name = name)
         }
     }
@@ -77,41 +79,30 @@ fun Greetings(names: List<String> = listOf("World", "Compose")) {
 @Composable
 fun Greeting(name: String) {
     Log.e(name)
-    Log.e(name)
     val expanded = remember { mutableStateOf(false) }
     val extraPadding = if (expanded.value) 48.dp else 0.dp
-    Log.e()
 
     Surface(
         color = MaterialTheme.colors.primary,
         modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
     ) {
-        Log.e()
         Row(Modifier.padding(24.dp)) {
-            Log.e()
             Column(
                 Modifier
                     .weight(1F)
                     .padding(bottom = extraPadding)
             ) {
-                Log.e()
                 Text("Hello")
                 Text(name)
             }
-            Log.e()
 
             OutlinedButton(onClick = {
-                Log.e()
                 expanded.value = !expanded.value
             }) {
-                Log.e()
                 Text(text = if (expanded.value) "Show less" else "Show more")
             }
-            Log.e()
         }
-        Log.e()
     }
-    Log.e()
 }
 
 
