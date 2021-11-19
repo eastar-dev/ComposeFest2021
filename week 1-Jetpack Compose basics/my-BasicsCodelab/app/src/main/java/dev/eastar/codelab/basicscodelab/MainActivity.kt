9deabe332a40dc11f6523f2c026544b66d5a1e2b
eastar <eastar.j@navercorp.com>
2021-11-19 19:55:05 +0900

    1 week step 11

--- week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
+++ week 1-Jetpack Compose basics/my-BasicsCodelab/app/src/main/java/dev/eastar/codelab/basicscodelab/MainActivity.kt
@@ -4,11 +4,15 @@ import android.log.Log
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,7 +32,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen { shouldShowOnboarding = false }
@@ -79,8 +83,14 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 @Composable
 fun Greeting(name: String) {
     Log.e(name)
-    val expanded = remember { mutableStateOf(false) }
-    val extraPadding = if (expanded.value) 48.dp else 0.dp
+    var expanded by rememberSaveable { mutableStateOf(false) }
+    val extraPadding by animateDpAsState(
+        if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
 
     Surface(
         color = MaterialTheme.colors.primary,
@@ -90,16 +100,16 @@ fun Greeting(name: String) {
             Column(
                 Modifier
                     .weight(1F)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text("Hello")
                 Text(name)
             }
 
             OutlinedButton(onClick = {
-                expanded.value = !expanded.value
+                expanded = !expanded
             }) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+                Text(text = if (expanded) "Show less" else "Show more")
             }
         }
     }
