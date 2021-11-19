99fba3b6619b110e9bb06262b457d736f11b14a9
eastar <eastar.j@navercorp.com>
2021-11-19 20:57:29 +0900

    2 week 2-1 3

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/dev/eastar/example/layoutscodelab/MainActivity.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/dev/eastar/example/layoutscodelab/MainActivity.kt
@@ -3,10 +3,11 @@ package dev.eastar.example.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import dev.eastar.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
@@ -21,6 +22,25 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+}
+
+@Composable
+fun PhotographerCard() {
+    Column {
+        Text("Alfred Sisley", fontWeight = FontWeight.Bold)
+        // LocalContentAlpha is defining opacity level of its children
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            Text("3 minutes ago", style = MaterialTheme.typography.body2)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutsCodelabTheme {
+        PhotographerCard()
     }
 }
 
