5d8da341f2d9a7c52d6d4c441eadb8de87b8f045
eastar <eastarj@gmail.com>
2021-11-28 15:11:12 +0900

    2 week 2-1 10-4

--- week 2-1-Layouts in JetpackCompose/app/src/main/java/dev/eastar/example/layoutscodelab/ConstraintLayoutCompose.kt
+++ week 2-1-Layouts in JetpackCompose/app/src/main/java/dev/eastar/example/layoutscodelab/ConstraintLayoutCompose.kt
@@ -1,13 +1,17 @@
 package dev.eastar.example.layoutscodelab
 
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
+import androidx.constraintlayout.compose.layoutId
 import dev.eastar.example.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 
@@ -16,7 +20,7 @@ fun ConstraintLayoutContent() {
     ConstraintLayout {
 
         // Create references for the composables to constrain
-        val (button1,button2, text) = createRefs()
+        val (button1, button2, text) = createRefs()
 
         Button(
             onClick = { /* Do something */ },
@@ -78,5 +82,50 @@ fun LargeConstraintLayout() {
 fun LargeConstraintLayoutPreview() {
     LayoutsCodelabTheme {
         LargeConstraintLayout()
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp) // Portrait constraints
+        } else {
+            decoupledConstraints(margin = 32.dp) // Landscape constraints
+        }
+
+        ConstraintLayout(constraints) {
+            Button(
+                onClick = { /* Do something */ },
+                modifier = Modifier.layoutId("button", "button")
+            ) {
+                Text("Button")
+            }
+
+            Text("Text", Modifier.layoutId("text", "text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DecoupledConstraintLayoutPreview() {
+    LayoutsCodelabTheme {
+        DecoupledConstraintLayout()
     }
 }
