fb3a84da36c87b90e4e0990de08ec51d2b4e302d
eastar <eastarj@gmail.com>
2021-12-10 22:15:51 +0900

    4-1 Testing in Jetpack Compose Step 4

--- week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/ui/components/TopAppBarKtTest.kt
+++ week 4-1-Testing in Jetpack Compose/app/src/androidTest/java/com/example/compose/rally/ui/components/TopAppBarKtTest.kt
@@ -4,6 +4,7 @@ import androidx.compose.material.Text
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
+import androidx.compose.ui.test.onNodeWithText
 import com.example.compose.rally.RallyScreen
 import org.junit.Assert.*
 
@@ -11,6 +12,7 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import java.util.*
 
 class TopAppBarKtTest {
 
@@ -52,5 +54,21 @@ class TopAppBarKtTest {
             .assertIsSelected()
 
 //        Thread.sleep(5000)
+    }
+
+    @Test
+    fun rallyTopAppBarTest_currentLabelExists() {
+        val allScreens = RallyScreen.values().toList()
+        composeTestRule.setContent {
+            RallyTopAppBar(
+                allScreens = allScreens,
+                onTabSelected = { },
+                currentScreen = RallyScreen.Accounts
+            )
+        }
+
+        composeTestRule
+            .onNodeWithContentDescription(RallyScreen.Accounts.name)
+            .assertExists()
     }
 }
